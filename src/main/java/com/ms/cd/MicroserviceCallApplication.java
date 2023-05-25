b0f5a93cfad3580c6255141cd6ee8c8b83536e8d
Sagar Karampuri <karampurisagar3@gmail.com>
2023-05-25 18:30:15 +0530

    Pushing project to git repository

---
+++ src/main/java/com/ms/cd/MicroserviceCallApplication.java
@@ -0,0 +1,13 @@
+package com.ms.cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCallApplication.class, args);
+	}
+
+}
